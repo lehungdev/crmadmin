d890797878936b9876ada92f7d347f9ea407101f
HungLV <lehung.hut@gmail.com>
2020-07-08 01:24:45 +0700

    no message

--- src/Installs/app/Controllers/LA/LanguageController.php
+++ src/Installs/app/Controllers/LA/LanguageController.php
@@ -3,7 +3,7 @@
  * Controller generated using IdeaGroup
  * Help: lehung.hut@gmail.com
  * CrmAdmin is open-sourced software licensed under the MIT license.
- * Developed by: Lehunghut IT Solutions
+ * Developed by: Lehungdev IT Solutions
  * Developer Website: http://ideagroup.vn
  */
 
@@ -12,17 +12,18 @@ namespace App\Http\Controllers\LA;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use Illuminate\Support\Str;
 use Auth;
 use DB;
 use Validator;
 use Datatables;
 use Collective\Html\FormFacade as Form;
-use Lehunghut\Crmadmin\Models\Module;
-use Lehunghut\Crmadmin\Models\ModuleFields;
+use Lehungdev\Crmadmin\Models\Module;
+use Lehungdev\Crmadmin\Models\ModuleFields;
 
 use App\Models\Language;
 
-class LanguagesController extends Controller
+class LanguageController extends Controller
 {
     public $show_action = true;
 
@@ -209,17 +210,19 @@ class LanguagesController extends Controller
         $fields_popup = ModuleFields::getModuleFields('Languages');
 
         for($i = 0; $i < count($data->data); $i++) {
+            $data->data[$i] =(array)$data->data[$i];
             for($j = 0; $j < count($listing_cols); $j++) {
                 $col = $listing_cols[$j];
-                if($fields_popup[$col] != null && starts_with($fields_popup[$col]->popup_vals, "@")) {
-                    $data->data[$i][$j] = ModuleFields::getFieldValue($fields_popup[$col], $data->data[$i][$j]);
+                $data->data[$i][$j] = $data->data[$i][$col];
+                if($fields_popup[$col] != null && Str::of($fields_popup[$col]->popup_vals)->startsWith('@')) {
+                    $data->data[$i][$j] = ModuleFields::getFieldValue($fields_popup[$col], $data->data[$i][$col]);
                 }
                 if($col == $module->view_col) {
-                    $data->data[$i][$j] = '<a href="' . url(config('crmadmin.adminRoute') . '/languages/' . $data->data[$i][0]) . '">' . $data->data[$i][$j] . '</a>';
+                    $data->data[$i][$j] = '<a href="' . url(config('crmadmin.adminRoute') . '/languages/' . $data->data[$i][$listing_cols[0]]) . '">' . $data->data[$i][$col] . '</a>';
                 }
                 if($fields_popup[$col] != null && $fields_popup[$col]->field_type_str == "Image") {
-                    if($data->data[$i][$j] != 0) {
-                        $img = \App\Models\Upload::find($data->data[$i][$j]);
+                    if($data->data[$i][$col] != 0) {
+                        $img = \App\Models\Upload::find($data->data[$i][$col]);
                         if(isset($img->name)) {
                             $data->data[$i][$j] = '<img src="'.$img->path().'?s=50">';
                         } else {
@@ -231,18 +234,18 @@ class LanguagesController extends Controller
                 }
 
                 // else if($col == "author") {
-                //    $data->data[$i][$j];
+                //    $data->data[$i][$col];
                 // }
             }
 
             if($this->show_action) {
                 $output = '';
                 if(Module::hasAccess("Languages", "edit")) {
-                    $output .= '<a href="' . url(config('crmadmin.adminRoute') . '/languages/' . $data->data[$i][0] . '/edit') . '" class="btn btn-warning btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-edit"></i></a>';
+                    $output .= '<a href="' . url(config('crmadmin.adminRoute') . '/languages/' . $data->data[$i][$listing_cols[0]] . '/edit') . '" class="btn btn-warning btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-edit"></i></a>';
                 }
 
                 if(Module::hasAccess("Languages", "delete")) {
-                    $output .= Form::open(['route' => [config('crmadmin.adminRoute') . '.languages.destroy', $data->data[$i][0]], 'method' => 'delete', 'style' => 'display:inline']);
+                    $output .= Form::open(['route' => [config('crmadmin.adminRoute') . '.languages.destroy', $data->data[$i][$listing_cols[0]]], 'method' => 'delete', 'style' => 'display:inline']);
                     $output .= ' <button class="btn btn-danger btn-xs" type="submit"><i class="fa fa-times"></i></button>';
                     $output .= Form::close();
                 }
